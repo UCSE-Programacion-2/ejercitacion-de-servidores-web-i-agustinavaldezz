1e507cf99beb2f4beee00c96be96a010a6dddc33
agustinavaldezz <agustina.valdez1809@gmail.com>
2026-05-24 01:01:54 -0300

    Completar servidor HTTP

--- src/server.js
+++ src/server.js
@@ -10,6 +10,25 @@ const http = require('http');
 function createServer() {
   const server = http.createServer((req, res) => {
     /* Aquí la lógica del enrutador */
+    const headers = { 'Content-Type': 'text/plain; charset=utf-8' }; //Definimos los encabezados que usaremos en todas las rutas
+
+    if (req.url === '/') {
+      res.writeHead(200, headers);
+      res.end('Bienvenid@s! Gracias por tu visita.');
+    } else if (req.url === '/nosotros') {
+      res.writeHead(200, headers);
+      res.end('Bienvenid@s a saber + de nosotros :)');
+    } else if (req.url === '/cursos') {
+      res.writeHead(200, headers);
+      res.end('Te interesan nuestros cursos. Vení a conocer la oferta!');
+    } else if (req.url === '/contacto') {
+      res.writeHead(200, headers);
+      res.end('Si querés contactarnos, hacelo a este Email: :)');
+    } else {
+      //cualquier otra ruta
+      res.writeHead(404, headers);
+      res.end('No se ha encontrado la ruta ingresada.');
+    }
   });
 
   return server;
